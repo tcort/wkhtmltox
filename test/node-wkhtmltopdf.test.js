e271cebba9925bbf896c2a04259249870bd4ee7d
Thomas Cort <linuxgeek@gmail.com>
2014-05-24 18:50:55 -0400

    node_wkhtmltopdf.cc: first pass at PDF rendering.
    Renders a PDF and returns it as a Buffer. This needs to be refined
    to accept args.

--- test/node-wkhtmltopdf.test.js
+++ test/node-wkhtmltopdf.test.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var expect = require('expect.js');
 var node_wkhtmltopdf = require('../index');
 
 describe('node-wkhtmltopdf', function () {
 	it ('should define a method called convert()', function () {
 		var result = node_wkhtmltopdf.convert();
-		expect(result).to.be('world');
+		fs.writeFileSync('test.pdf', result);
+		delete result;
 	});
 });
