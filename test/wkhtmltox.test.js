7070979b826961faabdbf5dda4b23084471f0582
Thomas Cort <linuxgeek@gmail.com>
2014-05-30 15:45:28 -0400

    wkhtmltox.test.js: formatting.

--- test/wkhtmltox.test.js
+++ test/wkhtmltox.test.js
@@ -1,61 +1,61 @@
 "use strict";
 
-var fs = require('fs');
-var path = require('path');
-var expect = require('expect.js');
-var wkhtmltox = require('../index');
+var fs = require("fs");
+var path = require("path");
+var expect = require("expect.js");
+var wkhtmltox = require("../index");
 
-describe('wkhtmltox', function () {
-	this.timeout(30000);
+describe("wkhtmltox", function() {
+    this.timeout(30000);
 
-	var converter = new wkhtmltox();
+    var kitchenSinkHtml = fs.readFileSync(path.join(__dirname, "input", "kitchen-sink.html"));
+    var converter = new wkhtmltox();
 
-	before(function (done) {
-		// Remove test output files prior to running the tests.
-		function cleanDir(dirPath) {
-			var files;
-			try {
-				files = fs.readdirSync(dirPath);
-			} catch (e) {
-				done(e);
-			}
-			if (files.length > 0) {
-				for (var i = 0; i < files.length; i++) {
-					var filePath = dirPath + '/' + files[i];
-					if (fs.statSync(filePath).isFile() && files[i][0] !== '.') {
-						fs.unlinkSync(filePath);
-					}
-				}
-			}
-		}
-		cleanDir(path.join(__dirname, 'output'));
+    before(function(done) {
+        function cleanDir(dirPath) {
+            var files;
+            try {
+                files = fs.readdirSync(dirPath);
+            } catch (e) {
+                done(e);
+            }
+            if (files.length > 0) {
+                for (var i = 0; i < files.length; i++) {
+                    var filePath = dirPath + "/" + files[i];
+                    if (fs.statSync(filePath).isFile() && files[i][0] !== ".") {
+                        fs.unlinkSync(filePath);
+                    }
+                }
+            }
+        }
+        cleanDir(path.join(__dirname, "output"));
+        setTimeout(done, 2000);
+    });
 
-		setTimeout(done, 2000); // wait a couple more seconds for the workers to start up.
-	});
+    describe("pdf()", function() {
+        it("should be defined", function() {
+            expect(converter).to.have.property("pdf");
+        });
+        it("should be a function", function() {
+            expect(converter.pdf).to.be.a("function");
+        });
+        it("should convert html to pdf", function(done) {
+            converter.pdf(kitchenSinkHtml, { pageSize: "letter" }).pipe(fs.createWriteStream(path.join(__dirname, "output", "kitchen-sink.pdf"))).on("finish", done);
+        });
+    });
 
-	describe('pdf()', function () {
-		it ('should be defined', function () {
-			expect(converter).to.have.property('pdf');
-		});
-		it ('should be a function', function () {
-			expect(converter.pdf).to.be.a('function');
-		});
-		it ('should convert html to pdf', function (done) {
-			converter.pdf(fs.readFileSync(path.join(__dirname, 'input', 'kitchen-sink.html')), { pageSize: 'letter' }).pipe(fs.createWriteStream(path.join(__dirname, 'output', 'kitchen-sink.pdf'))).on('finish', done);
-		});
-	});
-	describe('image()', function () {
-		it ('should be defined', function () {
-			expect(converter).to.have.property('image');
-		});
-		it ('should be a function', function () {
-			expect(converter.image).to.be.a('function');
-		});
-		it ('should convert html to png', function (done) {
-			converter.image(fs.readFileSync(path.join(__dirname, 'input', 'kitchen-sink.html')), { format: 'png' }).pipe(fs.createWriteStream(path.join(__dirname, 'output', 'kitchen-sink.png'))).on('finish', done);
-		});
-		it ('should convert html to jpg', function (done) {
-			converter.image(fs.readFileSync(path.join(__dirname, 'input', 'kitchen-sink.html')), { format: 'jpg' }).pipe(fs.createWriteStream(path.join(__dirname, 'output', 'kitchen-sink.jpg'))).on('finish', done);
-		});
-	});
+    describe("image()", function() {
+        it("should be defined", function() {
+            expect(converter).to.have.property("image");
+        });
+        it("should be a function", function() {
+            expect(converter.image).to.be.a("function");
+        });
+        it("should convert html to png", function(done) {
+            converter.image(kitchenSinkHtml, { format: "png" }).pipe(fs.createWriteStream(path.join(__dirname, "output", "kitchen-sink.png"))).on("finish", done);
+        });
+        it("should convert html to jpg", function(done) {
+            converter.image(kitchenSinkHtml, { format: "jpg" }).pipe(fs.createWriteStream(path.join(__dirname, "output", "kitchen-sink.jpg"))).on("finish", done);
+        });
+    });
 });
